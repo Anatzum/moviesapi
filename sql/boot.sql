5d728c1e4352c40c9e10eb8bbba8b9c361d80d24
Michael Whitener <mjawhitener@gmail.com>
2022-07-20 10:24:43 -0500

    Revert on format

--- sql/boot.sql
+++ sql/boot.sql
@@ -1,91 +1,149 @@
-CREATE TABLE languages(
-    id INT NOT NULL AUTO_INCREMENT,
-    iso VARCHAR(255) NOT NULL,
-    abbreviation VARCHAR(10) NOT NULL,
-    NAME VARCHAR(255) NOT NULL,
+CREATE TABLE languages (
+	id INT NOT NULL AUTO_INCREMENT,
+    iso varchar(255) NOT NULL,
+    abbreviation varchar(10) NOT NULL,
+    name varchar(255) NOT NULL,
+
     PRIMARY KEY(id)
-) ENGINE = INNODB; CREATE TABLE companies(
+) ENGINE=INNODB;
+
+CREATE TABLE companies (
     id INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL,
+    name varchar(255) NOT NULL,
+
     PRIMARY KEY(id)
-) ENGINE = INNODB; CREATE TABLE generes(
+) ENGINE=INNODB;
+
+CREATE TABLE generes (
     id INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL,
+    name varchar(255) NOT NULL,
+
     PRIMARY KEY(id)
-) ENGINE = INNODB; CREATE TABLE keywords(
+) ENGINE=INNODB;
+
+CREATE TABLE keywords (
     id INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL,
+    name varchar(255) NOT NULL,
+
     PRIMARY KEY(id)
-) ENGINE = INNODB; CREATE TABLE ratings(
+) ENGINE=INNODB;
+
+CREATE TABLE ratings (
     id INT NOT NULL AUTO_INCREMENT,
-    NAME VARCHAR(255) NOT NULL,
+    name varchar(255) NOT NULL,
+
     PRIMARY KEY(id)
-) ENGINE = INNODB; CREATE TABLE `cast`(
+) ENGINE=INNODB;
+
+CREATE TABLE `cast` (
     id INT NOT NULL AUTO_INCREMENT,
-    `character` VARCHAR(255) DEFAULT "unassigned" NOT NULL,
-    NAME VARCHAR(255) NOT NULL,
+    `character` varchar(255) DEFAULT "unassigned" NOT NULL,
+    name varchar(255) NOT NULL,
     `order` INT,
     movie_id INT NOT NULL,
+
     PRIMARY KEY(id),
     INDEX(movie_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE
-) ENGINE = INNODB; CREATE TABLE crew(
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE crew (
     id INT NOT NULL AUTO_INCREMENT,
-    department VARCHAR(255) DEFAULT "unassigned",
-    NAME VARCHAR(255) NOT NULL,
-    job VARCHAR(255) DEFAULT "unassigned",
+    department varchar(255) DEFAULT "unassigned",
+    name varchar(255) NOT NULL,
+    job varchar(255) DEFAULT "unassigned",
     movie_id INT NOT NULL,
+
     PRIMARY KEY(id),
     INDEX(movie_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE
-) ENGINE = INNODB; CREATE TABLE movies(
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE movies (
     id INT NOT NULL AUTO_INCREMENT,
     rating_id INT NOT NULL,
     budget INT,
-    homepage VARCHAR(255) DEFAULT "",
-    original_language VARCHAR(255) DEFAULT "",
-    original_title VARCHAR(255) DEFAULT "",
-    overview VARCHAR(255) DEFAULT "",
+    homepage varchar(255) DEFAULT "",
+    original_language varchar(255) DEFAULT "",
+    original_title varchar(255) DEFAULT "",
+    overview varchar(255) DEFAULT "",
     popularity FLOAT,
     release_date DATETIME,
     revenue FLOAT,
     runtime FLOAT,
-    STATUS VARCHAR
-        (255),
-        tagline VARCHAR(255),
-        title VARCHAR(255),
-        vote_average FLOAT,
-        vote_count INT,
-        PRIMARY KEY(id),
-        INDEX(rating_id),
-        FOREIGN KEY(rating_id) REFERENCES ratings(id) ON DELETE SET NULL
-) ENGINE = INNODB;
-CREATE TABLE movie_keyword(
+    status varchar(255),
+    tagline varchar(255),
+    title varchar(255),
+    vote_average FLOAT,
+    vote_count INT,
+
+    PRIMARY KEY(id),
+    INDEX(rating_id),
+    FOREIGN KEY (rating_id)
+        REFERENCES ratings(id)
+        ON DELETE SET NULL
+) ENGINE=INNODB;
+
+CREATE TABLE movie_keyword (
     movie_id INT NOT NULL,
     keyword_id INT NOT NULL,
-    INDEX(movie_id),
-    INDEX(keyword_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE,
-    FOREIGN KEY(keyword_id) REFERENCES keywords(id) ON DELETE CASCADE
-) ENGINE = INNODB; CREATE TABLE movie_genere(
+
+    INDEX (movie_id),
+    INDEX (keyword_id),
+
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (keyword_id)
+        REFERENCES keywords(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE movie_genere (
     movie_id INT NOT NULL,
     genere_id INT NOT NULL,
-    INDEX(movie_id),
-    INDEX(genere_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE,
-    FOREIGN KEY(genere_id) REFERENCES generes(id) ON DELETE CASCADE
-) ENGINE = INNODB; CREATE TABLE movie_company(
+
+    INDEX (movie_id),
+    INDEX (genere_id),
+
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (genere_id)
+        REFERENCES generes(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE movie_company (
     movie_id INT NOT NULL,
     company_id INT NOT NULL,
-    INDEX(movie_id),
-    INDEX(company_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE,
-    FOREIGN KEY(company_id) REFERENCES companies(id) ON DELETE CASCADE
-) ENGINE = INNODB; CREATE TABLE movie_language(
+
+    INDEX (movie_id),
+    INDEX (company_id),
+
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (company_id)
+        REFERENCES companies(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE movie_language (
     movie_id INT NOT NULL,
     language_id INT NOT NULL,
-    INDEX(movie_id),
-    INDEX(language_id),
-    FOREIGN KEY(movie_id) REFERENCES movies(id) ON DELETE CASCADE,
-    FOREIGN KEY(language_id) REFERENCES languages(id) ON DELETE CASCADE
-) ENGINE = INNODB;
+
+    INDEX (movie_id),
+    INDEX (language_id),
+
+    FOREIGN KEY (movie_id)
+        REFERENCES movies(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (language_id)
+        REFERENCES languages(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
